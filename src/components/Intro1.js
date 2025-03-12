0bd4b881e586793e3c3156a9b7ad4a20eb7beddb
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2025-03-12 12:00:26 -0400

    practicing making components

---
+++ src/components/Intro1.js
@@ -0,0 +1,15 @@
+function Intro1(){
+    return (
+        <div className="blog-post-intro">
+            <h2>I can code in React now</h2>
+            <div>
+                <p> I've completed the react
+                     basics course
+                </p>
+                <p className="link">Read More...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro1;
