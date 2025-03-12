60d18eeb0e17b701462672b3adad48cbf5e362ce
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2025-03-12 12:49:42 -0400

    learnt in line css styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,15 @@
 function Sidebar(props){
+    const asideStyle =  {
+        background: "azure",
+        width: "calc(30% - 10px)",
+        marginLeft: "10px",
+    }
     return(
-        <h1>{props.greet} from Sidebar!</h1>
+        <aside 
+        style={asideStyle}
+        className="sidebar-component">
+            <h1>{props.greet} from Sidebar!</h1>
+        </aside>
     );
 };
 
