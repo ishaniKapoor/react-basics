f07c54b84f85dc2ca319363cf0c3031fdd8aa46e
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2025-03-12 20:26:00 -0400

    learning basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import TextInputWithFocusButton from "./components/TextInputWithFocusButton";
+import Homepage from "./components/Homepage";
+import AboutMe from "./components/AboutMe";
+import {Routes, Route, Link} from "react-router-dom";
 
-function App(props) {
+function App() {
   return (
-    <TextInputWithFocusButton />
+  <>
+    <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/AboutMe" className="nav-item">About Me</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Homepage />}/>
+      <Route path="/AboutMe" element={<AboutMe />}/>
+    </Routes>
+  </>
   );
 }
 
