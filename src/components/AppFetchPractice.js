187ce2081e5a427fbd80d29addc4e731fd645111
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2025-03-15 17:01:11 -0400

    practicing fetch API

---
+++ src/components/AppFetchPractice.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function AppFetchPractice(){
+    const [user, setUser] = useState([]);
+
+    const fetchData = () => {
+        fetch("https://randomuser.me/api/?results=1")
+        .then(response => response.json())
+        .then(data => setUser(data));
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return Object.keys(user).length > 0 ? (
+        <div>
+            <h1>Data returned</h1>
+            <h2>First name: {user.results[0].name.first}</h2>
+            <h2>Last name: {user.results[0].name.last}</h2>
+        </div>
+    ) : (
+        <h1>Data pending...</h1>
+    )
+};
